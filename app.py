e5a890bdc07f5f43a7c987e877281bf209bfe3ed
jvsaez <jovisaib@gmail.com>
2024-07-04 12:15:30 +0200

    wip

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 import streamlit as st
 from groq import Groq
+import time
 
-client = Groq()
 
 st.title('GROQ Whisper V3 Demo - jovisaib.com')
 
 
-
+user_token = st.text_input("GROQ API token")
 audio_file = st.file_uploader("Upload Audio", type=["wav","mp3","m4a"])
 
 
-
 if st.sidebar.button("Transcribe Audio"):
-    if audio_file is not None:
+    if user_token == "":
+        st.sidebar.error("Please, input your GROQ API token.")
+    elif audio_file is not None:
+        client = Groq(api_key=user_token)
         st.sidebar.success("Transcribing audio...")
-        transcription = "adsasassa do request"
-        st.sidebar.success("Transcription complete!")
-        st.text(transcription["text"])
+        try:
+            start_time = time.time()
+            transcription = client.audio.transcriptions.create(
+                file=audio_file,
+                model="whisper-large-v3",
+                response_format="json",
+                temperature=0.0
+            )
+            end_time = time.time()
+            latency_ms = (end_time - start_time) * 1000
+            st.sidebar.success("Transcription complete!")
+            st.markdown(transcription.text)
+            st.text(f"Latency: {latency_ms:.2f} ms")
+        except Exception:
+            st.sidebar.error("Invalid GROQ token.")
     else:
-        st.sidebar.error("Please, upload an audio file.")
-# from streamlit_mic_recorder import mic_recorder
-# audio = mic_recorder(
-#     start_prompt="Start recording",
-#     stop_prompt="Stop recording",
-#     just_once=False,
-#     use_container_width=False,
-#     callback=None,
-#     args=(),
-#     kwargs={},
-#     key=None
-# )
+        st.sidebar.error("Please, upload a valid audio file.")
+
+
+st.sidebar.header("Play Audio File")
+st.sidebar.audio(audio_file)
